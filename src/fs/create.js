6cd0a6d18f7acc74c092c114d275fc061d85a92f
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-04 20:33:36 +0400

    feat: implemented create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,4 +1,16 @@
+import { writeFileSync, existsSync } from 'fs';
+
 export const create = async () => {
-     
-    
+     const file = 'files/fresh.txt';
+     const text = 'I am fresh and young';
+     const errorMessage = 'FS operation failed';
+
+    try {
+        const isFileExist = await existsSync(file);
+
+        if (isFileExist) throw new Error();
+        await writeFileSync(file, text);
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
