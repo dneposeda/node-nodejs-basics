8a7b1e7291d458be83640be6f92f84da928a935c
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 20:45:08 +0400

    feat: implemented args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,17 @@
+import { argv } from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    // set arguments in cli 'RSS_TAG=123123 RSS_VALUE=qweqwe node env.js'
+    const prefix = '--';
+    const argumentsCLI = {};
+
+    argv.forEach((val, index) => {
+        if (val.startsWith(prefix)) {
+            argumentsCLI[val.replace(prefix,'')] = argv[index+1]
+        }
+    });
+
+    console.log(Object.entries(argumentsCLI).map(([propName,value])=>`${propName} is ${value}`).join(', '))
 };
+
+parseArgs()
