81a19b8a9168c26f251b9628dfdcd973b833ac19
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 20:19:08 +0400

    feat: implemented env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,13 @@
+import { env } from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+    // set environment variables in cli 'RSS_TAG=123123 RSS_VALUE=qweqwe node env.js'
+    const prefix = 'RSS_';
+    const envVars = [];
+
+    for (const item in env) {
+        if (item.startsWith(prefix)) envVars.push(`${item}=${env[item]}`)
+    }
+
+    console.log(envVars.join('; '))
 };
