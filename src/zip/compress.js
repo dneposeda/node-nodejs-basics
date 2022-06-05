6ea1487a21f131c98bace34796cc69ce6fc4dd05
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 23:38:05 +0400

    feat: implement decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,7 +2,7 @@ import { dirname  } from 'path';
 import { fileURLToPath } from 'url';
 import { createReadStream, createWriteStream } from 'fs';
 import { createGzip } from 'zlib';
-import { pipeline } from 'stream/promises'
+import { pipeline } from 'stream/promises';
 
 export const compress = async () => {
     const __filename = fileURLToPath(import.meta.url);
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,22 @@
+import { dirname  } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
 export const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const archiveName = 'archive.gz';
+
+    const file = `${__dirname}/files/fileToCompress.txt`;
+    const archive = `${__dirname}/files/${archiveName}`;
+
+    const gunzip = createGunzip();
+    const source = createReadStream(archive);
+    const target = createWriteStream(file)
+
+    await pipeline(source, gunzip, target);
 };
+
+decompress();
