6a09cdd7228c873ddc3ec300e1c532158093fcda
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 19:08:23 +0400

    feat: implemented rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,19 @@
+import { existsSync, renameSync } from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+    const srcFile = 'files/wrongFilename.txt';
+    const destFile = 'files/properFilename.md';
+    const errorMessage = 'FS operation failed';
+
+    try {
+        const isSrcFileExist = await existsSync(srcFile);
+        const isDestFileExist = await existsSync(destFile);
+
+        if (!isSrcFileExist || isDestFileExist) throw new Error();
+
+        await renameSync(srcFile, destFile)
+
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
