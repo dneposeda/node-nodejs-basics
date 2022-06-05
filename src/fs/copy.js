bb4cfb37920c2adf064f167a7d1c9a9fefb4a472
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 19:08:11 +0400

    feat: implemented copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,28 @@
+import { existsSync, readdirSync, statSync, mkdirSync, copyFileSync, constants } from 'fs';
+
 export const copy = async () => {
-    // Write your code here 
+    const srcFolder = 'files';
+    const targetFolder = 'files_copy';
+    const errorMessage = 'FS operation failed';
+
+    try {
+        const isSrcFolderExist = await existsSync(srcFolder);
+        const isTargetFolderExist = await existsSync(targetFolder);
+
+        if (!isSrcFolderExist || isTargetFolderExist) throw new Error();
+
+        await mkdirSync(targetFolder)
+
+        const files = await readdirSync(srcFolder);
+
+        for (const item of files) {
+            const statusFile= await statSync(srcFolder+'/'+item)
+
+            if (statusFile.isFile()) {
+                await copyFileSync(srcFolder+'/'+item, targetFolder+'/'+item, constants.COPYFILE_EXCL);
+            }
+        }
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
