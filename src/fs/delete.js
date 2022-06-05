0ace6376dc932d595ec3365de888e6b47751e120
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 19:17:33 +0400

    feat: implemented delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
+import { existsSync, unlinkSync } from 'fs';
+
 export const remove = async () => {
-    // Write your code here 
+    const destFile = 'files/fileToRemove.txt';
+    const errorMessage = 'FS operation failed';
+
+    try {
+        const isDestFileExist = await existsSync(destFile);
+
+        if (!isDestFileExist) throw new Error();
+
+        await unlinkSync(destFile);
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
