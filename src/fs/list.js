bcd453f474069e20cba028b03d1ba6738b34c3c0
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 19:21:29 +0400

    feat: implemented list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
+import { existsSync, readdirSync } from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+    const folder = 'files';
+    const errorMessage = 'FS operation failed';
+
+    try {
+        const isFolderExist = await existsSync(folder);
+
+        if (!isFolderExist) throw new Error();
+
+        const files = await readdirSync(folder);
+
+        console.log(files)
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
