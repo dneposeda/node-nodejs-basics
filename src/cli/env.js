cb745d635e08055f90fd76615a8da83457821654
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 20:51:26 +0400

    refactor: change env

--- src/cli/env.js
+++ src/cli/env.js
@@ -3,11 +3,15 @@ import { env } from 'process';
 export const parseEnv = () => {
     // set environment variables in cli 'RSS_TAG=123123 RSS_VALUE=qweqwe node env.js'
     const prefix = 'RSS_';
-    const envVars = [];
+    const envVars = {};
 
     for (const item in env) {
-        if (item.startsWith(prefix)) envVars.push(`${item}=${env[item]}`)
+        if (item.startsWith(prefix)) {
+            envVars[item] = env[item]
+        }
     }
 
-    console.log(envVars.join('; '))
+    console.log(Object.entries(envVars).map(([name,value])=>`${name}=${value}`).join('; '))
 };
+
+parseEnv()
