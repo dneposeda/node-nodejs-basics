76d42b14afd0f5c857d0f51e26a66bda9ec7f9d8
Denis Tivikov <tivikov@mercurydevelopment.com>
2022-06-05 19:28:40 +0400

    feat: implemented read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,19 @@
+import { existsSync, readFileSync } from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+    const file = 'files/fileToRead.txt';
+    const errorMessage = 'FS operation failed';
+    const encoding = 'utf8';
+
+    try {
+        const isFileExist = await existsSync(file);
+
+        if (!isFileExist) throw new Error();
+
+        const content = await readFileSync(file, encoding);
+
+        console.log(content)
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
